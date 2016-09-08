7a218b5a04c4164d59d4a5f89c8712b474647e2a
Thomas M Santangelo <ts159@pvhs-lab112-d19.pvsd.org>
2016-09-08 14:29:08 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String s;
+        s = "name";
+        String s2 = "nickname";
+        System.out.println(s);
+        /*
+        things you cant name strings
+        reserved words
+        starting with number
+        special characters
+        spaces
+        reuse variable name
+         */
+        int a = 45;
+        double b = 3;
+        double sum = a + b;
+        a = 125125;
+        int e, f;
+        String string = "";
+        //strings have methods
+    }
+}
